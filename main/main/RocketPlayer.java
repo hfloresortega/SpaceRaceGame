a7a5200d43fe84233820560ec78a8598f0f8aca6
vanu122-Developer <pooltyzq@yandex.ru>
2025-10-31 15:24:40 -0700

    update constructor, update() function and draw().

--- main/main/RocketPlayer.java
+++ main/main/RocketPlayer.java
@@ -15,30 +15,96 @@
 *
 * <<Add more references here>>
 *
-* Version: 2025-10-29
+* Version: 2025-10-31
 */
 package main;
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
 
 public class RocketPlayer extends RocketShip
 { 
+	GameWindow gw;
+	KeyHandler keyH;
+	
     // set starting position using RocketShip's constructor
-    public RocketPlayer(int x, int y)
-    {
-        super(x, y); // call parent constructor
-    }
+	public RocketPlayer(GameWindow gw, KeyHandler keyH)
+	{
+		this.gw = gw;
+		this.keyH = keyH;		
+		setDefaultValues();
+		getPlayerImage();
+	}
+	// starting position of rocket
+	@Override
+	public void setDefaultValues() {
+		x = 100;
+		y = 500;
+		speed = 1;
+		directionFolder = "rocket";
+	}
+	
+	//Get player image
+	public void getPlayerImage() {
+		try {
+			rocket = ImageIO.read(getClass().getResourceAsStream("/rocket/Rocket.png"));
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void update() {
+		 // move player using keys and update sprite
+       if (keyH.upPressed) {
+    	   directionFolder = "rocket";
+           moveUp();
+       }
+       else if (keyH.downPressed) {
+    	   directionFolder = "rocket";
+           y += speed; // move down
+       }
+       else if (keyH.leftPressed) {
+    	   directionFolder = "rocket";
+       	x -= speed;
+       }
+       else if (keyH.rightPressed) {
+    	   directionFolder = "rocket";
+       	x += speed;
+       }
+       // could add left/right if needed
+	}
+	@Override
+	public void draw(Graphics2D g2) {
+		   // draw player rocket
+		BufferedImage image = null;
+		
+		switch(directionFolder) {
+		case "rocket":
+			image = rocket;
+			break;
+		}
+		
+		g2.drawImage(image, x, y, sizeWidth, sizeHeight, null);
+	}
+	
+	//Don't need because have in parent class RocketShip
+//    @Override
+//    // for collision detection
+//    public Rectangle getBounds() {
+//        return new Rectangle(x, y, sizeWidth, sizeHeight);
+//    }
 
-    @Override
-    // for collision detection
-    public Rectangle getBounds() {
-        return new Rectangle(x, y, width, height);
-    }
-
-    @Override
-    // resets player to a y position
-    public void resetPosition(int newY) {
-        y = newY;
-    }
+	//Don't need because have in parent class RocketShip
+//    @Override
+//    // resets player to a y position
+//    public void resetPosition(int newY) {
+//        y = newY;
+//    }
 
   
 }
