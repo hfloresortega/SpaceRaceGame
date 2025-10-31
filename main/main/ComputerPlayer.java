b7ab8f2b00eef910b0df8fd7756a8d6aed04875d
vanu122-Developer <pooltyzq@yandex.ru>
2025-10-31 15:20:49 -0700

    Add sprites change constructor. Add getPlayerImage, add setDefaultValue. Update draw system

--- main/main/ComputerPlayer.java
+++ main/main/ComputerPlayer.java
@@ -15,21 +15,38 @@
 *
 * <<Add more references here>>
 *
-* Version: 2025-10-29
+* Version: 2025-10-31
 */
 package main;
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class ComputerPlayer extends RocketShip
 {
 	private int direction = -1; // -1 means moving up, +1 means moving down
 
     // constructor
-    public ComputerPlayer(int x, int y)
-    {
-        super(x, y); // calls RocketShip constructor
-    }
+    public ComputerPlayer(GameWindow gw)
+	{
+		this.gw = gw;
 
+		setDefaultValues();
+		getPlayerImage();
+	}
+
+  //Get player image
+  	public void getPlayerImage() {
+  		try {
+  			rocket = ImageIO.read(getClass().getResourceAsStream("/rocket/RocketBot.png"));
+  		} catch(IOException e) {
+  			e.printStackTrace();
+  		}
+  	}
+    
     // makes the computer rocket move automatically
     public void autoMove()
     {
@@ -45,20 +62,48 @@ public class ComputerPlayer extends RocketShip
             direction = -1; // start moving up
         }
     }
+ // starting position of rocket change x coordinate
+    @Override
+	public void setDefaultValues() {
+		x = 500;
+		y = 500;
+		speed = 1;
+		directionFolder = "rocket";
+	}
+    
+    public void update() {
+    	directionFolder = "rocket";
+	}
     
     @Override
-    // for collision detection
-    public Rectangle getBounds()
-    {
-        return new Rectangle(x, y, width, height);
-    }
+	public void draw(Graphics2D g2) {
+		   // draw player rocket
+		BufferedImage image = null;
+		
+		switch(directionFolder) {
+		case "rocket":
+			image = rocket;
+			break;
+		}
+		
+		g2.drawImage(image, x, y, sizeWidth, sizeHeight, null);
+	}
+    
+  //Don't need because have in parent class RocketShip
+//    @Override
+//    // for collision detection
+//    public Rectangle getBounds()
+//    {
+//        return new Rectangle(x, y, sizeWidth, sizeHeight);
+//    }
 
-    @Override
-    // reset rocket position to specific y
-    public void resetPosition(int newY)
-    {
-        y = newY;
-    }
+  //Don't need because have in parent class RocketShip
+//    @Override
+//    // reset rocket position to specific y
+//    public void resetPosition(int newY)
+//    {
+//        y = newY;
+//    }
 
     
     
