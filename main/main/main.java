babc00e93fee35eaf5a48fc03b4f0c6742d919f2
vanu122-Developer <pooltyzq@yandex.ru>
2025-10-31 15:23:36 -0700

    start SpaceRaceGame, comment new MenuWindow

--- main/main/main.java
+++ main/main/main.java
@@ -1,10 +1,26 @@
 package main;
 
+/**
+* Lead Author(s):
+* @author Hassel Flores Ortega
+* @author Ivan Fesiunov
+* 
+* References:
+* Morelli, R., & Walde, R. (2016).
+* Java, Java, Java: Object-Oriented Problem Solving
+* https://open.umn.edu/opentextbooks/textbooks/java-java-java-object-oriented-problem-solving
+*
+
+*
+* Version: 2025-10-31
+*/
+
 import javax.swing.JFrame;
 
 public class main {
 	
 	public static void main(String[] args) {
-        new MenuWindow();
+		SpaceRaceGame game = new SpaceRaceGame();
+        //new MenuWindow();
     }
 }
