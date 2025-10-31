1e2499c8e3269806dac4773d12d3dfe7b220be3b
vanu122-Developer <pooltyzq@yandex.ru>
2025-10-31 15:16:39 -0700

    add sprite for asteroid fix size add setDefaultValues, getPlayerImage. Update draw, update.

--- main/main/Asteroid.java
+++ main/main/Asteroid.java
@@ -19,30 +19,64 @@
 */
 package main;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.Random;
+
+import javax.imageio.ImageIO;
 
 public class Asteroid
 {
+	GameWindow gw;
 	// asteroid position
 	int x;
 	int y;
 	
+	//Store to image data
+	public BufferedImage asteroid1;
+	public BufferedImage asteroid2;
+	public BufferedImage asteroid3;
+	//Folder picture
+	public String directionFolder;
+	
 	// size of asteroid
-	int width = 40;
-	int height = 40;
+	int sizeWidth = 25;
+	int sizeHeight = 20;
 	
 	// speed of asteroid
 	int speed = 1;
 	
 	Random random = new Random(); //makes asteroids appear randomly
 	
-	public Asteroid(int x, int y) // constructor to set position of asteroid
+	public Asteroid(GameWindow gw, int x, int y) // constructor to set position of asteroid
 	{
+		this.gw = gw;
 		this.x = x; 
 		this.y = y;
+		setDefaultValues();
 	}
 	
+	//Get asteroid image
+		public void getPlayerImage() {
+			try {
+				//Variant asteroids
+				asteroid1 = ImageIO.read(getClass().getResourceAsStream("/rocket/Asteroid.png"));
+				asteroid2 = ImageIO.read(getClass().getResourceAsStream("/rocket/Asteroid1.png"));
+				asteroid3 = ImageIO.read(getClass().getResourceAsStream("/rocket/Asteroid.png"));
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+	
+	// starting position of rocket
+	public void setDefaultValues() {
+		directionFolder = "asteroid1";
+		getPlayerImage();
+	}
+		
 	public void moveRandomly() // asteroid moves to left and resets when it leaves screen
 	{
 		x = x - speed;
@@ -58,7 +92,24 @@ public class Asteroid
 	
 	public Rectangle getBounds() // detects collison
 	{
-		return new Rectangle(x, y, width, height);
+		return new Rectangle(x, y, sizeWidth, sizeHeight);
+	}
+	
+	public void update() {
+		directionFolder = "asteroid1";
+	}
+	
+	public void draw(Graphics2D g2) {
+		 // draw asteroids
+		BufferedImage image = null;
+		
+		switch(directionFolder) {
+		case "asteroid1":
+			image = asteroid1;
+			break;
+		}
+		
+		g2.drawImage(image, x, y, sizeWidth, sizeHeight, null);
 	}
 }
 	
